d444cd533030a96af5e950d000cec4a812ce3b83
Tirsa St. Fort <lifelovelori@gmail.com>
2022-12-16 18:28:28 -0500

    commit tree 40786d080f8805d45e6c66f2fca94e8266c3c101

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+for(let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+}
+
+const gifts = [`teddy bear`, `drone`, `doll`]
+
+function wrapGifts(gifts) {
+    for(let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+    return gifts
+}
+
+wrapGifts(gifts)
+
+const messages = []
+
+function writeCards(names,event) {
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+}
+}

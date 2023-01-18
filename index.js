2c7f1bfae94aa2df234d9ed72c5137f80393a838
Tirsa St. Fort <lifelovelori@gmail.com>
2023-01-18 12:36:40 -0800

    Completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+}
+
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+    return gifts
+}
+
+wrapGifts(gifts)
+
+function writeCards(names,event){
+    const messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
